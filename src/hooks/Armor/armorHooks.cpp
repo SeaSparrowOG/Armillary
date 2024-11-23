4dacb83bc5d6d3c997609b87e1d30fb640cde377
SeaSparrowOG <spy.sakellariou@gmail.com>
2024-11-23 06:42:58 +0200

    Temporary leveling + project files

--- src/hooks/Armor/armorHooks.cpp
+++ src/hooks/Armor/armorHooks.cpp
@@ -14,6 +14,15 @@ namespace Hooks::Armor
 	{
 		REL::Relocation<std::uintptr_t> armorRating1(REL::ID(44014));
 
+		REL::Relocation<std::uintptr_t> advanceSkillTarget{ REL::ID(40488), 0x25 };
+		REL::Relocation<std::uintptr_t> incrementSkillTarget{ REL::ID(40489), 0x13 };
+
+		if (!REL::make_pattern<"E8">().match(advanceSkillTarget.address()) ||
+			!REL::make_pattern<"E9">().match(incrementSkillTarget.address())) {
+
+			util::report_and_fail("Armor: Failed to validate pattern, aborting launch.");
+		}
+
 		struct ratingPatch : Xbyak::CodeGenerator
 		{
 			ratingPatch(std::uintptr_t returnAddress)
@@ -37,21 +46,23 @@ namespace Hooks::Armor
 
 		auto& trampoline = SKSE::GetTrampoline();
 		trampoline.write_branch<6>(armorRating1.address() + 0xF9, trampoline.allocate(code1));
+		_handleSkillIncrease = trampoline.write_call<5>(advanceSkillTarget.address(), HandleSkillIncrease);
+		_handleSkillIncrement = trampoline.write_branch<5>(incrementSkillTarget.address(), HandleSkillIncrement);
 	}
 
 	float Manager::VisitArmorRating(RE::Character* a_target)
 	{
 		constexpr float armorFactor = 0.001f;
-		constexpr float diminishedFactor = 0.0005;
+		constexpr float diminishedFactor = 0.0005f;
 		constexpr float softCap = 700.0f;
 		constexpr float hardCap = 1000.0f;
 
-		const auto armorRating = a_target ? a_target->CalcArmorRating() : 0.0f;
+		const auto armorRating = a_target ? a_target->GetActorValue(RE::ActorValue::kDamageResist) : 0.0f;
 		const auto unbreakablePerk = Data::ModObject<RE::BGSPerk>("ARM_Armor_PRK_100_Unbreakable");
 		assert(unbreakablePerk);
 
 		auto response = 0.0f;
-
+		logger::debug("Armor: {}", armorRating);
 		if (a_target) {
 			if (armorRating > softCap) {
 				if (unbreakablePerk && a_target->HasPerk(unbreakablePerk)) {
@@ -68,9 +79,32 @@ namespace Hooks::Armor
 				}
 			}
 			else {
-				response = softCap * armorFactor;
+				response = armorRating * armorFactor;
 			}
 		}
 		return response;
+	}
+
+	void Manager::HandleSkillIncrease(RE::PlayerCharacter* a_char, RE::ActorValue a_skill, float a_ammount, RE::TESForm* a_advanceObject,
+		uint32_t a_advanceAction, bool a_useModifiers, bool a_hideNotification)
+	{
+		if (a_skill == RE::ActorValue::kLightArmor) {
+			_handleSkillIncrease(a_char, RE::ActorValue::kHeavyArmor, a_ammount, a_advanceObject, a_advanceAction,
+				a_useModifiers, a_hideNotification);
+		}
+		else {
+			_handleSkillIncrease(a_char, a_skill, a_ammount, a_advanceObject, a_advanceAction,
+				a_useModifiers, a_hideNotification);
+		}
+	}
+
+	void Manager::HandleSkillIncrement(RE::PlayerCharacter* a_player, RE::ActorValue a_skill, unsigned int a_amount)
+	{
+		if (a_skill == RE::ActorValue::kLightArmor) {
+			_handleSkillIncrement(a_player, RE::ActorValue::kHeavyArmor, a_amount);
+		}
+		else {
+			_handleSkillIncrement(a_player, a_skill, a_amount);
+		}
 	}
 }
--- src/hooks/Armor/armorHooks.h
+++ src/hooks/Armor/armorHooks.h
@@ -15,8 +15,13 @@ namespace Hooks
 
 		private:
 			static float VisitArmorRating(RE::Character* a_target);
+			static void HandleSkillIncrease(RE::PlayerCharacter* a_char, RE::ActorValue a_skill, float a_ammount, RE::TESForm* a_advanceObject,
+				uint32_t a_advanceAction, bool a_useModifiers, bool a_hideNotification);
+			static void HandleSkillIncrement(RE::PlayerCharacter* a_player, RE::ActorValue a_skill, unsigned int a_amount);
 
 			inline static REL::Relocation<decltype(&VisitArmorRating)> _visitArmorRating;
+			inline static REL::Relocation<decltype(&HandleSkillIncrease)> _handleSkillIncrease;
+			inline static REL::Relocation<decltype(&HandleSkillIncrement)> _handleSkillIncrement;
 		};
 	}
 }
